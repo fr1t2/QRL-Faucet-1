a134a594ab27b435238cfb4f65289c0007fa14da
fr1t2 <fr1t2@protonmail.com>
2020-12-06 01:37:25 -0700

    Refresh (#1)
    * Development and Site Build
    * Adding new site layout, Jekyll and re-working the faucet system
    * Latest Site Build

---
+++ script/mainnet_chainState.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Script grabs the curent state files, tar's them up, hashes the tar file and notarises 
+# it on the QRL blockchain through a message_tx.
+
+# This script requires 
+# - a fully synced node
+# - a QRL wallet with funds enough for the TX fee
+# - 
+# 
+
+epochNow=`date +%s`
+# uploadDir=/var/www/html/qrl.co.in/assets/QRL_state/
+uploadDir=/var/www/html/qrl.co.in/assets/state
+stateDir=/var/www/html/qrl/mainnet_state
+fileName="QRL_Mainnet_State.tar.gz"
+checkSumFileName="Mainnet_State_Checksums.txt"
+statsFileName="QRL_Node_Stats.json"
+user="fr1t2"
+
+mkdir $stateDir -p 
+
+# copy the files over
+rsync -a /home/$user/.qrl/data/state $stateDir
+
+# zip them up a little
+tar -czvf $uploadDir/$fileName $stateDir/*
+
+chainState=$(sudo -H -u $user /home/$user/.local/bin/qrl --json state)
+chainSize=$(du -hs /home/$user/.qrl/data/state/ | awk '{print $1}')
+tarFileSize=$(du -hs $uploadDir/$fileName | awk '{print $1}')
+
+# get the sha and md5 sums into a file
+echo "-------- SHA3-512 Sum --------" > ${uploadDir}/${checkSumFileName}
+openssl dgst -sha3-512 $uploadDir/$fileName >> ${uploadDir}/${checkSumFileName}
+echo "" >> ${uploadDir}/${checkSumFileName} 
+
+echo "-------- SHA3-256 Sum --------" >> ${uploadDir}/${checkSumFileName}
+openssl dgst -sha3-256 $uploadDir/$fileName >> ${uploadDir}/${checkSumFileName}
+echo "" >> ${uploadDir}/${checkSumFileName} 
+
+echo "-------- SHA-256 Sum --------" >> ${uploadDir}/${checkSumFileName}
+sha256sum $uploadDir/$fileName >> ${uploadDir}/${checkSumFileName}
+echo "" >> ${uploadDir}/${checkSumFileName} 
+
+echo "-------- MD5 Sum --------" >> ${uploadDir}/${checkSumFileName}
+md5sum  $uploadDir/$fileName >> ${uploadDir}/${checkSumFileName}
+
+
+# Write the STATS File for consumption by the server for data
+timestampJson="{ \"Unix_Timestamp\": \"$epochNow\" }"
+chainSizeJson="{ \"Uncompressed_Chain_Size\": \"$chainSize\" }"
+tarFileSizeJson="{ \"Tar_FileSize\": \"$tarFileSize\" }"
+
+echo "[" > ${uploadDir}/${statsFileName}
+echo $chainState "," >> ${uploadDir}/${statsFileName}
+
+echo $timestampJson "," >> ${uploadDir}/${statsFileName}
+echo $chainSizeJson "," >> ${uploadDir}/${statsFileName}
+echo $tarFileSizeJson >> ${uploadDir}/${statsFileName}
+echo "]" >> ${uploadDir}/${statsFileName}
+echo "" > ${uploadDir}/index.html
+
+# make sure you own the folder or are a part of the www-data group
+sudo chown www-data:www-data $uploadDir -R
---
+++ web/chain.md
@@ -0,0 +1,4 @@
+---
+title: chain
+layout: chain
+---
